eb3b5d141c49e4f61d8a5a052048839bb334d63a
Denis Malyshev <maldenser@gmail.com>
2023-02-27 20:49:06 +0300

    sqlite+alchemy integration

--- models.py
+++ models.py
@@ -1,13 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 
-from .processor import Base
-
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    surname = Column(String)
-    detailed_occupation = Column(String)
-    password = Column(String)
+    name = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
--- server.py
+++ server.py
@@ -1,14 +1,26 @@
-from typing import Union
+from typing import List
 
-from fastapi import FastAPI, Request, Query
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
+import crud, models, schemas
+from database import SessionLocal, engine
 
-class UserData(BaseModel):
-    name: str
-    surname: str
-    detailed_occupation: Union[str, None]
-    password: str
+models.Base.metadata.create_all(bind=engine)
+
+# class UserData(BaseModel):
+#     id: int
+#     name: str
+#     surname: str
+#     detailed_occupation: Union[str, None]
+#     password: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 app = FastAPI()
@@ -17,17 +29,26 @@ users_bd = {}
 
 
 @app.post("/sign_up")
-def create_item(data: UserData, test_param: bool = True):
-    user_id = max(users_bd, default=0) + 1
-    users_bd[user_id] = {
-        'name': data.name,
-        'surname': data.surname,
-        'detailed_occupation': data.detailed_occupation,
-        'password': data.password
-    }
-    return test_param
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_name(db, name=user.name)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+    # user_id = max(users_bd, default=0) + 1
+    # users_bd[user_id] = {
+    #     'name': data.name,
+    #     'surname': data.surname,
+    #     'detailed_occupation': data.detailed_occupation,
+    #     'password': data.password
+    # }
+    # return test_param
 
 
-@app.get("/give_all_bd")
-def give_all_bd():
-    return users_bd
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+# @app.get("/give_all_bd")
+# def give_all_bd():
+#     return users_bd
